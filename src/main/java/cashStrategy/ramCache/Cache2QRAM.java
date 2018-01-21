e5df499553a2910c1bef7ede5c249396f5dbb36d
Ivan Vorgul <ivan-kind@yandex.ru>
2018-01-21 20:14:40 +0300

    Правки, комментарии

--- src/main/java/cashStrategy/ramCache/Cache2QRAM.java
+++ src/main/java/cashStrategy/ramCache/Cache2QRAM.java
@@ -21,14 +21,17 @@ public class Cache2QRAM<Key, Value> implements ICache<Key, Value> {
 
     public Cache2QRAM(int size) {
         if (size < 10) {
+            // если размен меньше 10, то работает как LRU кэш
             cacheLvlMap = new HashMap<String, AbstractSimpleRamCache<Key, Value>>() {{
                 put(LOW_LVL, new CacheLRURAM<>(size));
-                put(TOP_LVL, new CacheLRURAM<Key, Value>(0));
-                put(MID_LVL, new CacheLRURAM<Key, Value>(0));
-               // put(MID_LVL, getEmptyCache());
+                put(TOP_LVL, new CacheLRURAM<>(0));
+                put(MID_LVL, new CacheLRURAM<>(0));
             }};
 
         } else {
+            // разделяем кэш след. образом:
+            // low и top уровни примерно по 20% от общего
+            // mid уровень примерно 60%
             int lowLvlSize = size / 5;
             cacheLvlMap = new HashMap<String, AbstractSimpleRamCache<Key, Value>>() {{
                 put(LOW_LVL, new CacheLRURAM<>(lowLvlSize));
@@ -59,10 +62,6 @@ public class Cache2QRAM<Key, Value> implements ICache<Key, Value> {
         return cacheLvlMap.containsKey(MID_LVL) ?
                 cacheLvlMap.get(MID_LVL).getOldestUsage()
                 : cacheLvlMap.get(LOW_LVL).getOldestUsage();
-    }
-
-    private void initEmptyCache() {
-
     }
 
     @Override
@@ -98,8 +97,10 @@ public class Cache2QRAM<Key, Value> implements ICache<Key, Value> {
             ICache<Key, Value> oneCache = cacheLvlMap.get(oneKey);
             if ((value = oneCache.getCachedObj(key)) != null) {
                 if (MID_LVL.equals(oneKey)) {
+                    // если искомый объект находится в mid уровне, то надо переложить его в top
                     AbstractSimpleRamCache<Key, Value> topLvlCache = cacheLvlMap.get(TOP_LVL);
                     if (topLvlCache.getMaxSize() == topLvlCache.getCurSize()) {
+                        // объекты вытолкнутые из top уровня перекладываем в low уровень с новым временем
                         Map.Entry<Key, Long> topLvlUsage = topLvlCache.getOldestUsage();
                         saveInCache(topLvlCache.deleteObj(topLvlUsage.getKey()), topLvlUsage.getKey());
                     }
@@ -127,8 +128,10 @@ public class Cache2QRAM<Key, Value> implements ICache<Key, Value> {
 
     @Override
     public Value saveInCache(Value objToCash, Key key) {
+        // все новые объекты ложим в low уровень
         AbstractSimpleRamCache<Key, Value> lowLvlCache = cacheLvlMap.get(LOW_LVL);
         if (lowLvlCache.getMaxSize() == lowLvlCache.getCurSize() && cacheLvlMap.get(MID_LVL).getMaxSize() != 0) {
+            // объекты, вытолкнутые из low уровня,перекладываем в mid уровень
             Map.Entry<Key, Long> usageEntry = lowLvlCache.getOldestUsage();
             cacheLvlMap.get(MID_LVL).saveInCache(lowLvlCache.deleteObj(usageEntry.getKey()),
                     usageEntry.getKey(), usageEntry.getValue());
@@ -161,57 +164,14 @@ public class Cache2QRAM<Key, Value> implements ICache<Key, Value> {
 
     @Override
     public void setSize(int size) {
-        //cacheLvlMap.values().forEach(cache -> cache.setSize());
         if (size < 10) {
-            //cacheLvlMap = ImmutableMap.of(LOW_LVL, new CacheLRURAM<>(size));
             cacheLvlMap.get(LOW_LVL).setSize(size);
         } else {
             int lowLvlSize = size / 5;
             cacheLvlMap.get(LOW_LVL).setSize(lowLvlSize);
             cacheLvlMap.get(TOP_LVL).setSize(lowLvlSize);
             cacheLvlMap.get(MID_LVL).setSize(size - 2 * lowLvlSize);
-            /*cacheLvlMap = ImmutableMap.of(
-                    LOW_LVL, new CacheLRURAM<>(lowLvlSize),
-                    MID_LVL, new CacheLRURAM<>(size - 2 * lowLvlSize),
-                    TOP_LVL, new CacheLRURAM<>(lowLvlSize));*/
         }
 
     }
-
-    /*class MidLvlCash<KeyLvl, ValueLvl> extends CacheLRURAM<KeyLvl, ValueLvl> {
-
-        private MidLvlCash<KeyLvl, ValueLvl> cashToReplace;
-
-        public MidLvlCash(int size, MidLvlCash<KeyLvl, ValueLvl> cashToReplace) {
-            super(size);
-            this.cashToReplace = cashToReplace;
-        }
-
-        private ValueLvl saveInCache(ValueLvl objToCache, KeyLvl keyLvl, long time) {
-            if ()
-        }
-
-        @Override
-        public ValueLvl saveInCache(ValueLvl objToCache, KeyLvl keyLvl, long time) {
-            if (this.getMaxSize() == this.cacheTable.size()) {
-
-            }
-            return super.saveInCache(objToCache, keyLvl, time);
-        }
-
-        @Override
-        public ValueLvl removeOldObj() {
-            KeyLvl keyToRemove = getOldestUsage().getKey();
-            long minTime = Long.MAX_VALUE;
-            for (Map.Entry<KeyLvl, Long> oneEntry : usageMap.entrySet()) {
-                if (minTime > oneEntry.getValue()) {
-                    minTime = oneEntry.getValue();
-                    keyToRemove = oneEntry.getKey();
-                }
-            }
-
-            usageMap.remove(keyToRemove);
-            return cacheTable.remove(keyToRemove);
-        }
-    }*/
 }
